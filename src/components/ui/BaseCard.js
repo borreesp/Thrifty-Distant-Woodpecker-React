4beb292979b4510fb64e640bc8ce845cf1601204
borreesp <luisborrero17@hotmail.com>
2025-11-29 19:46:44 +0100

    delete CRA

--- src/components/ui/BaseCard.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './BaseCard.css'
-
-const BaseCard = ({ children, className = '' }) => {
-  return <div className={`base-card ${className}`}>{children}</div>
-}
-
-export default BaseCard
